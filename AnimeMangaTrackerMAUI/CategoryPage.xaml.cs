352456fce8cddb9cf87dc2feb64c829e7b2df81e
ANDREEA BĂRĂIAN <andreea.baraian@stud.ubbcluj.ro>
2026-01-11 14:49:16 +0200

    Proiect MAUI in folder separat sper

---
+++ AnimeMangaTrackerMAUI/CategoryPage.xaml.cs
@@ -0,0 +1,34 @@
+using AnimeMangaTrackerMAUI.Models;
+
+namespace AnimeMangaTrackerMAUI;
+
+public partial class CategoryPage : ContentPage
+{
+	public CategoryPage()
+	{
+		InitializeComponent();
+	}
+    protected override async void OnAppearing()
+	{
+		base.OnAppearing();
+		if (BindingContext == null)
+		{
+			BindingContext = new Category();
+			listView.ItemsSource = await App.Database.GetCategoriesAsync();
+        }
+	}
+
+	async void OnSaveButtonClicked(object sender, EventArgs e)
+	{
+		var category = (Category)BindingContext;
+		await App.Database.SaveCategoryAsync(category);
+		listView.ItemsSource = await App.Database.GetCategoriesAsync();
+    }
+
+	async void OnDeleteButtonClicked(object sender, EventArgs e)
+	{
+		var category = (Category)BindingContext;
+		await App.Database.DeleteCategoryAsync(category);
+		listView.ItemsSource = await App.Database.GetCategoriesAsync();
+    }
+}
